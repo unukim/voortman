57c6654b0ea42495ba3e14a6ff5db95538abb3a0
unukim <unukim@icloud.com>
2025-12-08 21:27:30 +0100

    .

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 from ADXL345 import adxl345
 import time
 from datetime import datetime
+import csv
 
 sensor = adxl345(int_pin =17)
+model = "old"
 data =[]
 
 first_sample = sensor.read_fifo()
 start_time = first_sample['timestamp']
+start_time_date = datetime.fromtimestamp(start_time)
 data.append(first_sample)
 
 while True:
 	sample = sensor.read_fifo()
 	if sample:
 		data.append(sample)
-		elasped = (sample['timestamp'] - start_time).total_seconds()
-		if elasped >= 1.0:
+		now = time.time()
+		sample["datetime"] = datetime.fromtimestamp(sample["timestamp"]).strftime("%Y-%m-%d %H:%M:%S.%f")
+		if now - start_time >= 1.0:
 			break
+print(len(data), "data collected for", now - start_time, "seconds")
 
-print(data[0:15])
-print(data[-1])
-print(len(data))
+#save to csv file
+filename = f"{model}_{start_time_date}.csv"
+with open(filename, mode ='w', newline='') as file:
+	writer = csv.DictWriter(file, fieldnames = ["datetime", "timestamp", "x", "y", "z"])
+	writer.writeheader()
+	writer.writerows(data)
+
+
