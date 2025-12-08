dea85532737b7f95791b5223068a39264f732c77
unukim <unukim@icloud.com>
2025-12-08 17:08:34 +0100

    .

--- ADXL345.py
+++ ADXL345.py
@@ -1,5 +1,6 @@
 import spidev
 import time
+from datetime import datetime
 
 class adxl345:
 
@@ -9,6 +10,8 @@ class adxl345:
 		self.spi.open(0,0)
 		self.spi.mode = 3
 		self.spi.max_speed_hz = 5000000
+		self.sample_rate = 3200
+		self.dt = 1.0 / self.sample_rate
 		
 			
 		#Defind register hex
@@ -18,10 +21,12 @@ class adxl345:
 		self.POWER_CTL = 0x2D #set measuring mode
 		self.DATAX0 = 0x32
 		self.FIFO_CTL = 0x38
+		self.FIFO_STATUS =0x39
 		
 		self.write_register(self.POWER_CTL, 0x08) # start measuring 
 		self.write_register(self.BW_RATE, 0x0F) #set the sampling rate to max(3.2k hz)
 		self.write_register(self.FIFO_CTL, 0x80) #enable fifo stream mode
+		self.write_register(self.DATA_FORMAT, 0x08) #full resolution mode
 		
 	
 	def write_register(self, register, value):
@@ -30,22 +35,33 @@ class adxl345:
 	
 
 	
-	def read_register(self, register):
-		cmd = 0xC0 | register #0xC0 ensures read and multi-bytes sampling mode
+	def read_register_single(self, register):
+		cmd = 0x80 | register #0x80 ensures read and single byte sampling mode
 		resp = self.spi.xfer2([cmd] + [0]*6) # send commend & register byte + 6 dummy bytes
+		
+		return resp[1]
+		
+	def read_register_multi(self, register, length):
+		cmd = 0xC0 | register #0xC0 ensures read and multi-byte sampling mode
+		resp = self.spi.xfer2([cmd] + [0x00]*length) # send commend & register byte + 6 dummy bytes
 		
 		#returns bytes without the first(dummy) byte
 		return resp[1:]
-	
-	def measure(self):
-		fifo_status = read_register(0x39)  & 0x3F
-		timestamp = time.time()
-		response = self.read_register(self.DATAX0)
-		#shift high byte to left and combine with low byte
-		x = (response[1] << 8) | response[0]
-		y = (response[3] << 8) | response[2]
-		z = (response[5] << 8) | response[4]
+	def to_signed(self,val):
+		if val & 0x8000:
+			return val - 65536
+		return val
 
+	def measure(self):
+		fifo_status = self.read_register_single(self.FIFO_STATUS) & 0x3F
+		timestamp = time.time()
+		data = self.read_register_multi(self.DATAX0,6)
+		#shift high byte to left and combine with low byte
+		x = self.to_signed(data[1] << 8 | data[0]) *0.0039
+		y = self.to_signed(data[3] << 8 | data[2]) *0.0039
+		z = self.to_signed(data[5] << 8 | data[4]) *0.0039
+		
+		time.sleep(self.dt)
 		return {
 			'timestamp': timestamp,
 			'x': x,
